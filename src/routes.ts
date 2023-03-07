1346629714be4ec947f8002d0a45bbeb97c3d511
lucas5g <lucasdesousa19@hotmail.com>
2023-03-07 11:48:47 -0300

    test file routes

--- src/routes.ts
+++ src/routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express'
 
 export const routes = Router()
 
-// routes.get('/', (req, res) =>  res.json({message: 'Api release 2023-03-04'}))
+routes.get('/', (req, res) =>  res.json({message: 'Api release 2023-03-04 file routes'}))
 
 routes.get('/users', (req, res) => {
   res.json({message: 'list users'})
--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(cors())
 
-app.get('/', (req, res) => res.json({message:'Api release cyclic'}))
+// app.get('/', (req, res) => res.json({message:'Api release cyclic'}))
 app.use(routes)
 const port = process.env.PORT || 8001
 app.listen(port, ()=> console.log(`Server run http://localhost:${port}`))
