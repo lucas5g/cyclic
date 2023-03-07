05696e1d1d28bec6ae886c9a4cd639827f75adb5
lucas5g <lucasdesousa19@hotmail.com>
2023-03-07 11:51:45 -0300

    test pokemons

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import axios from 'axios'
 
 export const routes = Router()
 
@@ -6,4 +7,10 @@ routes.get('/', (req, res) =>  res.json({message: 'Api release 2023-03-04 file r
 
 routes.get('/users', (req, res) => {
   res.json({message: 'list users'})
+})
+
+routes.get('/pokemons', async (req, res) => {
+  const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto')
+
+  return res.json(data)
 })
